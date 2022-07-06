ea0720f550f0910c061f9fde5786c492a834968a
Jeeva18 <108772096+Jeeva18@users.noreply.github.com>
2022-07-06 06:09:36 +0530

    Add files via upload

---
+++ apti.js
@@ -0,0 +1,17 @@
+function check(){
+	var c=0;
+	var q1=document.apti.question1.value;
+	var q2=document.apti.question2.value;
+	var q3=document.apti.question3.value;
+	var q4=document.apti.question4.value;
+	var q5=document.apti.question5.value;
+	
+	if(q1=="10kmph") {c++}
+	if(q2=="25") {c++}
+	if(q3=="TANGLED") {c++}
+	if(q4=="700") {c++}
+	if(q5=="Father") {c++}
+
+
+	document.write(c)
+}
